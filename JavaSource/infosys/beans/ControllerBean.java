016d3727b148f428237629ff3b9bd1f8e5b79f9c
joeFong <joemanfong@gmail.com>
2016-10-21 08:59:51 -0700

    Changing Passwords now implemented.

--- JavaSource/infosys/beans/ControllerBean.java
+++ JavaSource/infosys/beans/ControllerBean.java
@@ -8,6 +8,8 @@ import javax.enterprise.context.ConversationScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import ca.bcit.infosys.employee.Employee;
+
 @Named("controller")
 @ConversationScoped
 public class ControllerBean implements Serializable{
@@ -15,6 +17,11 @@ public class ControllerBean implements Serializable{
     EmployeeLister list;
     String userName;
     String password;
+    
+    Integer empId;
+    String oldPassword;
+    String newPassword;
+    String confirmPassword;
 
     @PostConstruct
     public void init() {
@@ -25,28 +32,23 @@ public class ControllerBean implements Serializable{
         list = new EmployeeLister();
     }
     
-    /*
-    public String verifyLogin() {
-        for(Employee e: list.getEmployees()) {
-            if((getUserName().equals(e.getUserName())) &&
-                    (getPassword().equals(list.getLoginCombos().get(e.getUserName())))) {
-                return "next";
-            }
-        }
-        return "stay";
-    }
-    */
     
     public String verifyLogin() {
-        String verified = "";
-        try {
-            if(list.getLogInfo().get(getUserName()).equals(getPassword())) {
-                verified = "next";
-            }
-        } catch(NullPointerException n) {
-            return "stay";
+        
+        for(Employee e: list.getEmployees()) {
+            if(e.getEmpNumber() == 0000) 
+                if((getUserName().equals(e.getUserName())) &&
+                        (getPassword().equals(list.getLoginCombos().get(e.getUserName())))) 
+                    return "admin";   
         }
-        return verified;
+        
+        for(Employee e: list.getEmployees()) {
+            if((getUserName().equals(e.getUserName())) &&
+                    (getPassword().equals(list.getLoginCombos().get(e.getUserName())))) 
+                return "next"; 
+        }
+        
+        return "stay";
     }
     
     public String logOut() {
@@ -63,10 +65,64 @@ public class ControllerBean implements Serializable{
     public String getPassword() {
         return password;
     }
+    public Integer getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(Integer empId) {
+        this.empId = empId;
+    }
+
+    public String getOldPassword() {
+        return oldPassword;
+    }
+
+    public void setOldPassword(String oldPassword) {
+        this.oldPassword = oldPassword;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
     public void setPassword(String password) {
         this.password = password;
     }
        
-    
+    public String goToPassword() {
+        return "changePassword";
+    }
+    public String changePassword() {
+        String result = "";
+        for(Employee e: list.getEmployees()) {
+            if(e.getEmpNumber() == getEmpId()) {
+                if(list.getCred(e).getPassword().equals(getPassword()))
+                    if(getConfirmPassword().equals(getNewPassword())) {
+                        list.getCred(e).setPassword(getNewPassword());
+                        System.out.println(list.getCred(e).getPassword());
+                        list.getLogInfo().put(e.getUserName(), getNewPassword());
+                        System.out.println(list.getLogInfo().get(e.getUserName()));
+                        setPassword(getNewPassword());
+                        System.out.println(getPassword());
+                        result = "success";
+                        break;
+                    }
+            } else 
+                result = "fail";
+        }
+        return result;
+    }
     
 }
--- JavaSource/infosys/beans/EmployeeLister.java
+++ JavaSource/infosys/beans/EmployeeLister.java
@@ -13,11 +13,21 @@ import ca.bcit.infosys.employee.*;
 @ApplicationScoped
 public class EmployeeLister implements EmployeeList {
 
-
+    ArrayList<Credentials> cred = new ArrayList<Credentials>();
     ArrayList<Employee> employees = new ArrayList<Employee>();
     Map<String, String> logInfo = new HashMap<String, String>();
     
+    
     public EmployeeLister() {
+        Employee admin = new Employee("Admin", 0000, "Admin"); 
+        addEmployee(admin);
+        Credentials adminCred = new Credentials();
+        adminCred.setUserName(admin.getUserName());
+        adminCred.setPassword("default");
+        
+        logInfo.put(adminCred.getUserName(), adminCred.getPassword());
+        cred.add(adminCred);
+               
         Employee e1 = new Employee("John", 12345, "employeeJohn"); 
         addEmployee(e1);
         Credentials c = new Credentials();
@@ -25,16 +35,30 @@ public class EmployeeLister implements EmployeeList {
         c.setPassword("default");
         
         logInfo.put(c.getUserName(), c.getPassword());
-        
+        cred.add(c);
         
         Employee e2 = new Employee("Jovina", 12346, "employeeLow"); 
         addEmployee(e2);
         Credentials c2 = new Credentials();
         c2.setUserName(e2.getUserName());
         c2.setPassword("default");
+        cred.add(c2);
         
         logInfo.put(c2.getUserName(), c2.getPassword());
         
+    }
+    
+    public void addCred(Credentials c) {
+        cred.add(c);
+    }
+    
+    public Credentials getCred(Employee e) {
+        for(Credentials c: cred) {
+            if(e.getUserName() == c.getUserName()) {
+                return c;
+            }
+        }
+        return null;
     }
     
     public Map<String, String> getLogInfo() {
@@ -61,7 +85,11 @@ public class EmployeeLister implements EmployeeList {
 
     @Override
     public Employee getAdministrator() {
-        // TODO Auto-generated method stub
+        for(Employee e: employees) {
+            if(e.getEmpNumber() == 0000) {
+                return e;
+            }
+        }
         return null;
     }
 
@@ -100,5 +128,7 @@ public class EmployeeLister implements EmployeeList {
     public Employee getCurrentEmployee(ArrayList<Employee> e, int i) {
         return e.get(i);
     }
+    
+    
 
 }
